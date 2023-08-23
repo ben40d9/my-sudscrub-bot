bbe0b157a32a911e92be5431f8e386d2cddb60f5
ben40d9 <benpaley6@gmail.com>
2023-08-23 15:08:06 -0700

    get response function made

---
+++ pages/api/getResponse.js
@@ -0,0 +1,28 @@
+import { connectToDatabase } from "../../src/utils/mongodb/db.js";
+import { generateResponse } from "../../src/utils/generateResponse.js";
+
+export default async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  const { comment } = req.body;
+
+  // Connect to the MongoDB
+  const { db } = await connectToDatabase();
+
+  // Send the response back to the frontend
+  const { response, continuation, topComments } = await generateResponse(
+    db,
+    comment
+  );
+
+  console.log(
+    "Response from generateResponse:",
+    response,
+    continuation,
+    topComments
+  );
+
+  res.json({ response, continuation, topComments });
+};
