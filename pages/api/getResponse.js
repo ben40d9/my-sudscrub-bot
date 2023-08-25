4e8a943a9fda5d88b330315a9a3b719b6a408b1e
ben40d9 <benpaley6@gmail.com>
2023-08-24 19:10:43 -0700

    try/catch block added for error handling

--- pages/api/getResponse.js
+++ pages/api/getResponse.js
@@ -2,28 +2,36 @@ import { connectToDatabase } from "../../src/utils/mongodb/db.js";
 import { generateResponse } from "../../src/utils/generateResponse.js";
 
 const getResponse = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).end();
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).end();
+    }
+
+    const { comment } = req.body;
+
+    // Connect to the MongoDB
+    const { db } = await connectToDatabase();
+
+    // Send the response back to the frontend
+    const { response, continuation, topComments } = await generateResponse(
+      comment
+    );
+
+    console.log(
+      "Response from generateResponse:",
+      response,
+      continuation,
+      topComments
+    );
+
+    res.json({ response, continuation, topComments });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error: "An unexpected error occurred while generating the response.",
+      });
   }
-
-  const { comment } = req.body;
-
-  // Connect to the MongoDB
-  const { db } = await connectToDatabase();
-
-  // Send the response back to the frontend
-  const { response, continuation, topComments } = await generateResponse(
-    comment
-  );
-
-  console.log(
-    "Response from generateResponse:",
-    response,
-    continuation,
-    topComments
-  );
-
-  res.json({ response, continuation, topComments });
 };
 
 export default getResponse;
