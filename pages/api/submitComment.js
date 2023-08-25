b18e1908fd9ffcb4028f0a8e44912c0ec5076d1d
ben40d9 <benpaley6@gmail.com>
2023-08-24 19:11:10 -0700

    more clear error handling in the console

--- pages/api/submitComment.js
+++ pages/api/submitComment.js
@@ -18,7 +18,11 @@ const submitComment = async (req, res) => {
     } catch (error) {
       res
         .status(500)
-        .json({ success: false, message: "An error occurred.", error });
+        .json({
+          success: false,
+          message: "An error occurred while submitting the comment:",
+          error,
+        });
     }
   } else {
     res.status(405).json({ success: false, message: "Method not allowed." });
