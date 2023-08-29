ebaec550777757e9c08beb1b75e28eea86a45a9c
ben40d9 <benpaley6@gmail.com>
2023-08-28 21:33:40 -0700

    updated the handleFeedbackSubmission function so it now does the following: Updates the feedback state with the new feedback. Sets the isLoading state to true, indicating that an API request is in progress. Makes an API request to /api/getResponse to regenerate the bot's response based on the new feedback. Updates the response state with the new response, which should cause the UI to re-render. Resets the isLoading state to false.

--- pages/index.js
+++ pages/index.js
@@ -1,103 +1,118 @@
 // // pages/index.js
 // import { useState } from "react";
-// import ResponseDisplay from "../components/ResponseDisplay";
-// import TopComments from "../components/TopComments";
-// import FeedbackForm from "../components/FeedbackForm";
+// import CommentInput from "../components/CommentInput.js"; // Import the CommentInput component
+// import ResponseDisplay from "../components/ResponseDisplay.js";
+// import TopComments from "../components/TopComments.js";
+// import FeedbackForm from "../components/FeedbackForm.js";
 
 // export default function Home() {
-//   // State to manage user comment, bot's response, continuation, and top comments
+//   // State to manage user comment, bot's response, continuation, top comments, and loading
 //   const [comment, setComment] = useState("");
 //   const [response, setResponse] = useState("");
-//   const [continuation, setContinuation] = useState("");
-//   const [topComments, setTopComments] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [feedback, setFeedback] = useState("");
 
 //   const handleCommentSubmit = async () => {
+//     if (comment.trim() === "") return;
+
+//     setIsLoading(true); // Set loading to true
+
 //     const result = await fetch("/api/getResponse", {
 //       method: "POST",
 //       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ comment }),
+//       body: JSON.stringify({ comment, feedback }),
 //     });
 //     const data = await result.json();
 //     console.log("Data from server:", data);
 //     setResponse(data.response);
-//     setContinuation(data.continuation);
-//     setTopComments(data.topComments);
+
+//     setIsLoading(false);
 //   };
 
-//   const handleFeedbackSubmit = (feedbackData) => {
-//     fetch("/api/submitFeedback", {
+//   const handleFeedbackSubmit = async (feedbackData) => {
+//     setFeedback(feedbackData.feedback); // Store feedback
+
+//     const result = await fetch("/api/getResponse", {
 //       method: "POST",
 //       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(feedbackData),
+//       body: JSON.stringify({ comment, feedback: feedbackData.feedback }),
 //     });
+//     const data = await result.json();
+//     setResponse(data.response); // Update the response state
+//   };
+
+//   const handleClear = () => {
+//     setComment("");
+//     setResponse("");
+//     setFeedback("");
 //   };
 
 //   return (
-//     <div>
+//     <div className="container">
 //       <h1>Sud Scrub Bot</h1>
-//       <div>
-//         <h2>Enter your comment for the bot to respond to:</h2>
-//         <input
-//           type="text"
-//           value={comment}
-//           onChange={(e) => setComment(e.target.value)}
-//           placeholder="Enter comment"
-//         />
-//         <button onClick={handleCommentSubmit}>Submit Comment</button>
-//         <button onClick={() => setComment("")}>Clear Comment</button>
-//       </div>
-//       {response && (
-//         <ResponseDisplay response={response} continuation={continuation} />
-//       )}
-//       {topComments && topComments.length > 0 && (
-//         <TopComments comments={topComments} />
-//       )}
-//       <FeedbackForm onSubmit={handleFeedbackSubmit} />
+//       <CommentInput
+//         comment={comment}
+//         setComment={setComment}
+//         onSubmit={handleCommentSubmit}
+//       />
+//       {comment && <button onClick={handleClear}>Clear</button>}
+//       {/* {comment && (
+//         <button onClick={handleCommentSubmit}>Regenerate Response</button>
+//       )} */}
+//       {isLoading && <div className="loading-spinner"></div>}{" "}
+//       {/* Loading spinner */}
+//       {response && <ResponseDisplay response={response} />}
+//       {response && <FeedbackForm onSubmit={handleFeedbackSubmit} />}
 //     </div>
 //   );
 // }
 
-// pages/index.js
 import { useState } from "react";
-import CommentInput from "../components/CommentInput.js"; // Import the CommentInput component
+import CommentInput from "../components/CommentInput.js";
 import ResponseDisplay from "../components/ResponseDisplay.js";
-import TopComments from "../components/TopComments.js";
 import FeedbackForm from "../components/FeedbackForm.js";
 
 export default function Home() {
-  // State to manage user comment, bot's response, continuation, top comments, and loading
   const [comment, setComment] = useState("");
   const [response, setResponse] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [feedback, setFeedback] = useState("");
 
+  // handles comment submission
   const handleCommentSubmit = async () => {
     if (comment.trim() === "") return;
 
-    setIsLoading(true); // Set loading to true
+    setIsLoading(true);
 
     const result = await fetch("/api/getResponse", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ comment, feedback }),
     });
+
     const data = await result.json();
-    console.log("Data from server:", data);
     setResponse(data.response);
 
     setIsLoading(false);
   };
 
+  // handles feedback submission
   const handleFeedbackSubmit = async (feedbackData) => {
-    setFeedback(feedbackData.feedback); // Store feedback
+    setFeedback(feedbackData.feedback); // Update the feedback state
 
-    const result = await fetch("/api/getResponse", {
+    setIsLoading(true);
+
+    // Make API call to regenerate the response
+    const result = await fetch("/api/submitFeedback", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ comment, feedback: feedbackData.feedback }),
     });
+
     const data = await result.json();
-    setResponse(data.response); // Update the response state
+    setResponse(data.response);
+
+    setIsLoading(false);
   };
 
   const handleClear = () => {
@@ -115,11 +130,10 @@ export default function Home() {
         onSubmit={handleCommentSubmit}
       />
       {comment && <button onClick={handleClear}>Clear</button>}
-      {/* {comment && (
+      {comment && response && (
         <button onClick={handleCommentSubmit}>Regenerate Response</button>
-      )} */}
-      {isLoading && <div className="loading-spinner"></div>}{" "}
-      {/* Loading spinner */}
+      )}
+      {isLoading && <div className="loading-spinner"></div>}
       {response && <ResponseDisplay response={response} />}
       {response && <FeedbackForm onSubmit={handleFeedbackSubmit} />}
     </div>
