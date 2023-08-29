cdd1383807246c5570374552ea303bbfdefa0e10
ben40d9 <benpaley6@gmail.com>
2023-08-28 23:37:37 -0700

    refactor(pages): update index.js to include getRefinedResponse API call and UI improvements

--- pages/index.js
+++ pages/index.js
@@ -67,6 +67,87 @@
 //   );
 // }
 
+//------
+//---
+//------
+//---
+//------
+
+// import { useState } from "react";
+// import CommentInput from "../components/CommentInput.js";
+// import ResponseDisplay from "../components/ResponseDisplay.js";
+// import FeedbackForm from "../components/FeedbackForm.js";
+
+// export default function Home() {
+//   const [comment, setComment] = useState("");
+//   const [response, setResponse] = useState("");
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [feedback, setFeedback] = useState("");
+
+//   // handles comment submission
+//   const handleCommentSubmit = async () => {
+//     if (comment.trim() === "") return;
+
+//     setIsLoading(true);
+
+//     const result = await fetch("/api/getResponse", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ comment, feedback }),
+//     });
+
+//     const data = await result.json();
+//     setResponse(data.response);
+
+//     setIsLoading(false);
+//   };
+
+//   // handles feedback submission
+//   const handleFeedbackSubmit = async (feedbackData) => {
+//     setFeedback(feedbackData.feedback); // Update the feedback state
+
+//     setIsLoading(true);
+
+//     // Make API call to regenerate the response
+//     const result = await fetch("/api/submitFeedback", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ comment, feedback: feedbackData.feedback }),
+//     });
+
+//     const data = await result.json();
+//     setResponse(data.response);
+
+//     setIsLoading(false);
+//   };
+
+//   const handleClear = () => {
+//     setComment("");
+//     setResponse("");
+//     setFeedback("");
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1>Sud Scrub Bot</h1>
+//       <CommentInput
+//         comment={comment}
+//         setComment={setComment}
+//         onSubmit={handleCommentSubmit}
+//       />
+//       {comment && <button onClick={handleClear}>Clear</button>}
+//       {comment && response && (
+//         <button onClick={handleCommentSubmit}>Regenerate Response</button>
+//       )}
+//       {isLoading && <div className="loading-spinner"></div>}
+//       {response && <ResponseDisplay response={response} />}
+//       {response && <FeedbackForm onSubmit={handleFeedbackSubmit} />}
+//       {isLoading && <div className="loading-spinner"></div>}
+//     </div>
+//   );
+// }
+
+// pages/index.js
 import { useState } from "react";
 import CommentInput from "../components/CommentInput.js";
 import ResponseDisplay from "../components/ResponseDisplay.js";
@@ -77,6 +158,7 @@ export default function Home() {
   const [response, setResponse] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [feedback, setFeedback] = useState("");
+  const [score, setScore] = useState(5);
 
   // handles comment submission
   const handleCommentSubmit = async () => {
@@ -84,10 +166,11 @@ export default function Home() {
 
     setIsLoading(true);
 
+    // Fetch initial response
     const result = await fetch("/api/getResponse", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ comment, feedback }),
+      body: JSON.stringify({ comment }),
     });
 
     const data = await result.json();
@@ -98,15 +181,21 @@ export default function Home() {
 
   // handles feedback submission
   const handleFeedbackSubmit = async (feedbackData) => {
-    setFeedback(feedbackData.feedback); // Update the feedback state
+    setFeedback(feedbackData.feedback);
+    setScore(feedbackData.score);
 
     setIsLoading(true);
 
-    // Make API call to regenerate the response
-    const result = await fetch("/api/submitFeedback", {
+    // Fetch refined response based on feedback
+    const result = await fetch("/api/getRefinedResponse", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ comment, feedback: feedbackData.feedback }),
+      body: JSON.stringify({
+        comment,
+        originalResponse: response,
+        feedback: feedbackData.feedback,
+        score: feedbackData.score,
+      }),
     });
 
     const data = await result.json();
@@ -119,6 +208,7 @@ export default function Home() {
     setComment("");
     setResponse("");
     setFeedback("");
+    setScore(5);
   };
 
   return (
