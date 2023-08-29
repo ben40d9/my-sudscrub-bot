3dda78af55c170db7fedc25d99dc6060def80da5
ben40d9 <benpaley6@gmail.com>
2023-08-28 23:37:19 -0700

    feat(components): add FeedbackForm with UI improvements and rating explanation

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,4 +1,3 @@
-// // components/FeedbackForm.js
 // import { useState } from "react";
 
 // export default function FeedbackForm({ onSubmit }) {
@@ -15,7 +14,7 @@
 //     <div className="feedback-form">
 //       <h2>
 //         Provide feedback to help improve the response and then rate the
-// response:
+//         response:
 //       </h2>
 //       <input
 //         type="text"
@@ -60,14 +59,56 @@ export default function FeedbackForm({ onSubmit }) {
         onChange={(e) => setFeedback(e.target.value)}
         placeholder="Enter feedback"
       />
-      <select value={score} onChange={(e) => setScore(Number(e.target.value))}>
-        {[1, 2, 3, 4, 5].map((value) => (
-          <option key={value} value={value}>
-            {value}
-          </option>
-        ))}
-      </select>
+      <div className="rating-wrapper">
+        <label htmlFor="score">Rate the response: </label>
+        <select
+          id="score"
+          value={score}
+          onChange={(e) => setScore(Number(e.target.value))}
+        >
+          <option value="1">1 - Terrible response</option>
+          <option value="2">2 - Decent response</option>
+          <option value="3">3 - Neutral response</option>
+          <option value="4">4 - Good response</option>
+          <option value="5">5 - Perfect response</option>
+        </select>
+      </div>
       <button onClick={handleFeedbackSubmit}>Submit Feedback</button>
+
+      <style jsx>{`
+        .feedback-form {
+          background-color: #f2f2f2;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+        .rating-wrapper {
+          display: flex;
+          align-items: center;
+          margin-top: 10px;
+        }
+        label {
+          margin-right: 10px;
+          font-weight: bold;
+        }
+        select {
+          padding: 5px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+        button {
+          margin-top: 20px;
+          padding: 10px 20px;
+          background-color: #007bff;
+          color: white;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+        button:hover {
+          background-color: #0056b3;
+        }
+      `}</style>
     </div>
   );
 }
