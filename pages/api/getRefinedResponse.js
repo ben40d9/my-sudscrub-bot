9e0d0d7da0f43e043b794338f5c27c382851892f
ben40d9 <benpaley6@gmail.com>
2023-08-28 22:52:52 -0700

    In this file, we import the generateRefinedResponse function we just created. This function takes in the original comment, original response, feedback, and score from the client-side and generates a refined response.
    We're using a POST request for this API call. The request body should contain the original comment, original response, feedback, and score.

---
+++ pages/api/getRefinedResponse.js
@@ -0,0 +1,32 @@
+// pages/api/getRefinedResponse.js
+import { generateRefinedResponse } from "../../src/utils/generateRefinedResponse.js";
+
+const getRefinedResponse = async (req, res) => {
+  try {
+    // Validate the request method
+    if (req.method !== "POST") {
+      return res.status(405).end();
+    }
+
+    // Destructure data from the request body
+    const { comment, originalResponse, feedback, score } = req.body;
+
+    // Generate a refined response using the new function
+    const { response } = await generateRefinedResponse(
+      comment,
+      originalResponse,
+      feedback,
+      score
+    );
+
+    // Send the refined response back to the client
+    res.status(200).json({ response });
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({
+      error: "An unexpected error occurred while processing the feedback.",
+    });
+  }
+};
+
+export default getRefinedResponse;
